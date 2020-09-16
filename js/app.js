60b0c684150406d4f435b35e15022fcf90231e66
ritech94 <53955765+ritech94@users.noreply.github.com>
2020-09-15 23:05:05 -0400

    Final Upload (Hopefully)

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ function rem(x = 1) {
 $(document).ready(() => {
     // Auto Populate Copyright Year
     let year = new Date().getFullYear();
-    $('#year').text(year);
+    $('#copyright_year').text(year);
 
     // Auto-Change Order Text
     $('#stencil-phrase-input').on('keyup', () => {
@@ -21,7 +21,7 @@ $(document).ready(() => {
     // Auto-Change Stencil Design
     $('#design-select').on('change', () => {
         let val = $('#design-select').val();
-        let img_src = val != '' && val < 7 ? `./assets/Horse${val}.png` : `./assets/Horse1.png`;
+        let img_src = val != '' && val < 7 ? `./assets/products/Horse${val}.png` : `./assets/products/Horse1.png`;
         $('#stencil-image').attr('src', img_src);
     })
 
@@ -39,7 +39,7 @@ $(document).ready(() => {
         event.preventDefault();
 
         // Pre-populate modal
-        let image_src = ($('#design-select').val() > 0) ? `./assets/Horse${$('#design-select').val()}.png` : `./assets/Horse1.png`;
+        let image_src = ($('#design-select').val() > 0) ? `./assets/products/Horse${$('#design-select').val()}.png` : `./assets/products/Horse1.png`;
         let design = ($('#design-select').val() > 0) ? $('#design-select option:selected').text() : 'Grazing Horse';
         let name = $('#stencil-phrase-input').val();
 
@@ -50,6 +50,112 @@ $(document).ready(() => {
         // Show Order Modal
         $('#order-modal').modal('show');
     })
+
+    // Manual Order Submission
+    $('#order_modal_submit').on('click', () => {
+        $('#order_modal_form').submit();
+    })
+
+    // Order Submission
+    $('#order_modal_form').on('submit', event => {
+        // Prevent default submission actions
+        event.preventDefault();
+
+        // Default EmailJS Parameters
+        let subject = '';
+        let message = '';
+        let to_email = '';
+        let from_name = '';
+
+        // Load order form details
+        let delivery_type = 'PICKUP';
+        let name = $('#order-name').val();
+        let email = $('#order_email').val();
+        let tel = $('#order_tel').val();
+        let street = '';
+        let city = '';
+        let province = '';
+        let postal_code = '';
+
+        if ($('#order_radio_1').is(':checked')) {
+            delivery_type = 'DELIVERY';
+            street = $('#order-street').val();
+            city = $('#order-city').val();
+            province = $('#order-province').val();
+            postal_code = $('#order-postal-code').val();
+        }
+
+        subject = `Incoming Order [${delivery_type}]`;
+        if (delivery_type === 'PICKUP') {
+            message = `Customer Name: ${name}<br>Phone Number: ${tel}<br>Email: ${email}<br><br>Horse Type: ${$('#modal-design-name').html()}<br>Custom Name: ${$('#modal-custom-name').html()}`;
+        } else {
+            message = `Customer Name: ${name}<br>Phone Number: ${tel}<br>Email: ${email}<br><br>Shipping:<br>Address: ${street}, ${city}, ${province}<br>Postal Code: ${postal_code}<br><br>Horse Type: ${$('#modal-design-name').html()}<br>Custom Name: ${$('#modal-custom-name').html()}`;
+        }
+        to_email = `farrier.rik@gmail.com`;
+        from_name = `Website Order Form`;
+
+        // console.log(`${subject}\n${message}\n${to_email}\n${from_name}`);
+
+        let templateParams = {
+            subject: subject,
+            message: message,
+            to_email: to_email,
+            from_name: from_name
+        }
+
+        emailjs.send('gmail', 'template_4ql2coAj', templateParams)
+            .then(response => {
+                console.log('SUCCESS!', response.status, response.text);
+            }, error => {
+                console.log('FAILED...', error);
+            });
+
+        // Add extra messages for clients
+        subject = `Order Receipt`;
+        to_email = to_email;
+        message += `<br><br><hr><br>Price:<br>Cost: $20.00<br>HST: $2.60<br>Total: $22.60<br><br>If you have any inqueries, please email Rik at farrier.rik@gmail.com.`;
+
+        templateParams = {
+            subject: subject,
+            message: message,
+            to_email: email,
+            from_name: from_name
+        }
+
+        emailjs.send('gmail', 'template_4ql2coAj', templateParams)
+            .then(response => {
+                console.log('SUCCESS!', response.status, response.text);
+                alert('Order Submitted');
+                location.reload();
+            }, error => {
+                console.log('FAILED...', error);
+            });
+    })
+
+    $('#contact-form').on('submit', event => {
+        event.preventDefault();
+
+        // Collect contact information
+        let name = $('#contact_name').val();
+        let email = $('#contact_email').val();
+        let message = $('#contact_message').val();
+
+        let templateParams = {
+            subject: `Message from ${name}`,
+            message: `Name: ${name}<br>Email: ${email}<br>Message: ${message}`,
+            to_email: `farrier.rik@gmail.com`,
+            from_name: name
+        }
+
+        emailjs.send('gmail', 'template_4ql2coAj', templateParams)
+            .then(response => {
+                console.log('SUCCESS!', response.status, response.text);
+                alert('Message Sent!');
+                location.reload();
+            }, error => {
+                console.log('FAILED...', error);
+            });
+    }) 
 })
 
 // Smooth Scroll to Anchor
